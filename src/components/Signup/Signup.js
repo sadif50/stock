5b04065bcf512d7e5424fe38e4db4a71879ce68c
S. M. Sujan <smsujon50@gmail.com>
2022-12-08 13:08:19 +0600

    Sign up page design complete

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import rocket from '../../assets/rocket.png';
 
 const Signup = () => {
     return (
-        <div>
-            Sign Up
+        <div className='d-flex justify-content-center align-items-center bg-login-signup'>
+            <div className='card'>
+                <div className='text-center mb-2'>
+                    <img src={rocket} alt="" className='w-75'/>
+                    <h3 className='mt-2'>Create Account</h3>
+                    <p>Please Sign up for Stock Account.</p>
+                </div>
+                <form>
+                    <input type="email" className='form-control mb-4' placeholder='Email Address' />
+                    <input type="password" className='form-control mb-4' placeholder='Password' />
+                    <input type="submit" value="SIGN UP" className='btn btn-custom' />
+                </form>
+                <p className='m-0 pt-3'>Already have an account? Please <Link to='/login' className='text-decoration-none'><strong>Login</strong></Link> </p>
+            </div>
         </div>
     );
 };
