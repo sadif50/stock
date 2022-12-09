d38b0ece488b66ca579ffc4a524a26fc3882d057
S. M. Sujan <smsujon50@gmail.com>
2022-12-09 12:32:30 +0600

    Data show on table

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,97 +68,132 @@ const Home = () => {
         return <h1>Loading...</h1>
     }
     return (
-        <div>
-            <Plot className='myChart'
-                data={[
-                    {
-                        x: keys,
-                        y: opens,
-                        name: 'Open',
-                        type: 'scatter'
-                    },
-                    {
-                        x: keys,
-                        y: highs,
-                        name: 'High',
-                        yaxis: 'y2',
-                        type: 'scatter'
-                    },
-                    {
-                        x: keys,
-                        y: lows,
-                        name: 'Low',
-                        yaxis: 'y3',
-                        type: 'scatter'
-                    },
-                    {
-                        x: keys,
-                        y: closes,
-                        name: 'Close',
-                        yaxis: 'y4',
-                        type: 'scatter'
-                    },
-                    {
-                        x: keys,
-                        y: volumes,
-                        name: 'Volume',
-                        yaxis: 'y5',
-                        type: 'scatter'
-                    }
-                ]}
-                layout={
-                    {
-                        title: `${symbol} showing ${interval} interval`,
-                        autosize: true,
-                        height: 500,
-                        paper_bgcolor: '#eee',
-                        xaxis: { domain: [0.3, 0.7] },
-                        yaxis: {
-                            title: 'Open',
-                            titlefont: { color: '#1f77b4' },
-                            tickfont: { color: '#1f77b4' }
-                        },
-                        yaxis2: {
-                            title: 'High',
-                            titlefont: { color: '#ff7f0e' },
-                            tickfont: { color: '#ff7f0e' },
-                            anchor: 'free',
-                            overlaying: 'y',
-                            side: 'left',
-                            position: 0.20
-                        },
-                        yaxis3: {
-                            title: 'Low',
-                            titlefont: { color: '#2ca02c' },
-                            tickfont: { color: '#2ca02c' },
-                            anchor: 'free',
-                            overlaying: 'y',
-                            side: 'left',
-                            position: 0.10
-                        },
-                        yaxis4: {
-                            title: 'Close',
-                            titlefont: { color: '#d62728' },
-                            tickfont: { color: '#d62728' },
-                            anchor: 'x',
-                            overlaying: 'y',
-                            side: 'right',
-                            position: 0.75
-                        },
-                        yaxis5: {
-                            title: 'Volume',
-                            titlefont: { color: '#9467bd' },
-                            tickfont: { color: '#9467bd' },
-                            anchor: 'free',
-                            overlaying: 'y',
-                            side: 'right',
-                            position: 0.85
+        <div className="container">
+            <div className='row'>
+                <div>Hello</div>
+                <div className='col-12'>
+                    <Plot className='myChart'
+                        data={[
+                            {
+                                x: keys,
+                                y: opens,
+                                name: 'Open',
+                                type: 'scatter'
+                            },
+                            {
+                                x: keys,
+                                y: highs,
+                                name: 'High',
+                                yaxis: 'y2',
+                                type: 'scatter'
+                            },
+                            {
+                                x: keys,
+                                y: lows,
+                                name: 'Low',
+                                yaxis: 'y3',
+                                type: 'scatter'
+                            },
+                            {
+                                x: keys,
+                                y: closes,
+                                name: 'Close',
+                                yaxis: 'y4',
+                                type: 'scatter'
+                            },
+                            {
+                                x: keys,
+                                y: volumes,
+                                name: 'Volume',
+                                yaxis: 'y5',
+                                type: 'scatter'
+                            }
+                        ]}
+                        layout={
+                            {
+                                title: `${symbol} showing ${interval} interval`,
+                                autosize: true,
+                                height: 500,
+                                paper_bgcolor: '#eee',
+                                xaxis: { domain: [0.3, 0.7] },
+                                yaxis: {
+                                    title: 'Open',
+                                    titlefont: { color: '#1f77b4' },
+                                    tickfont: { color: '#1f77b4' }
+                                },
+                                yaxis2: {
+                                    title: 'High',
+                                    titlefont: { color: '#ff7f0e' },
+                                    tickfont: { color: '#ff7f0e' },
+                                    anchor: 'free',
+                                    overlaying: 'y',
+                                    side: 'left',
+                                    position: 0.20
+                                },
+                                yaxis3: {
+                                    title: 'Low',
+                                    titlefont: { color: '#2ca02c' },
+                                    tickfont: { color: '#2ca02c' },
+                                    anchor: 'free',
+                                    overlaying: 'y',
+                                    side: 'left',
+                                    position: 0.10
+                                },
+                                yaxis4: {
+                                    title: 'Close',
+                                    titlefont: { color: '#d62728' },
+                                    tickfont: { color: '#d62728' },
+                                    anchor: 'x',
+                                    overlaying: 'y',
+                                    side: 'right',
+                                    position: 0.75
+                                },
+                                yaxis5: {
+                                    title: 'Volume',
+                                    titlefont: { color: '#9467bd' },
+                                    tickfont: { color: '#9467bd' },
+                                    anchor: 'free',
+                                    overlaying: 'y',
+                                    side: 'right',
+                                    position: 0.85
+                                }
+                            }
                         }
-                    }
-                }
-            />
-            <div className='table-responsive'>
-                
+                        useResizeHandler
+                        style={{width: "100%", height: "100%"}}
+                    ></Plot>
+                </div>
+                <div className="col-12">
+                    <hr />
+                    <h4 className='text-center'>Showing {symbol}, {interval} Interval Data In Table</h4>
+                    <hr />
+                    <div className='table-responsive'>
+                        <table className="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>SL</th>
+                                    <th>Open</th>
+                                    <th>Close</th>
+                                    <th>High</th>
+                                    <th>Low</th>
+                                    <th>Volume</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    keys.map((key, i) => <tr>
+                                        <td>{i + 1}</td>
+                                        <td>{opens[i]}</td>
+                                        <td>{closes[i]}</td>
+                                        <td>{highs[i]}</td>
+                                        <td>{lows[i]}</td>
+                                        <td>{volumes[i]}</td>
+                                    </tr>)
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     );
